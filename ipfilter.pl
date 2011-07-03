3e74dfc1409b51d49d6e9d8b3d1e7b604ad3eb9b
Richard Scott Smith <scott@thescene.com>
2011-07-03 11:56:06 -0700

    Initial import into Git repository.  This is a new utility repository for useful one off Perl scripts.
    ipfilter is a Perl utility that counts then number of
    occurrences of an IP address, usually supplied from a
    log file.  Then is sorts them by frequency and outputs
    each IP address with the number of times it appears
    in the file/logs.

---
+++ ipfilter.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+# Filename: ipfilter.pl
+# Date: 8-15-2006
+# Author: Richard Scott Smith
+
+# The keys in the hash tables are by ip address
+# Hash table to store the number of times an ip address has occurred
+%iptable;
+
+# Temporary variable to hold the current ip address
+$ipaddress;
+
+# Count up the total number of IP addresses that have been processed
+$totalNumberOfIPAddresses = 0;
+
+while (<>) {
+  if (&identifyIPAddress()) {
+    if (exists $iptable{"$ipaddress"}) {
+      #print "IP was already in the hash table.\n";
+      $iptable{"$ipaddress"} +=1;
+    } else {
+      $iptable{"$ipaddress"} = 1;
+    } # End if-else
+
+    #print "$_\n";
+    #print "Total: $numberOfIPAddresses\n";
+  } # End if
+
+} # End while
+
+# Sort the iptable frequency into a list
+@ipaddress = sort by_frequency keys %iptable;
+
+# Finally print the contents of the iptable
+print "IP Table:\n";
+foreach (@ipaddress) {
+  # Local spaces for nicely printed output
+  my ($spaces) = " ";
+  print "$_";
+  for ($i = length $_; $i < 20; $i++) {
+    $spaces .= "-";
+  } # End for loop
+  $spaces .= " ";
+  print $spaces . $iptable{"$_"};
+  print "\n";
+} # End foreach
+
+print "\n";
+print "Total IP addresses scanned: $numberOfIPAddresses\n"; 
+
+# Identify if a line has an IP address on it
+sub identifyIPAddress {
+  if (/\d+\.\d+\.\d+\.\d+/) {
+    $numberOfIPAddresses += 1;
+    $ipaddress = $&;
+    #print "Here's an IP: $&\n";
+    return 1;
+  } else {
+    return 0;
+  } # End if-else
+} # End identifyIPAddress
+
+# Sort value (by frequency)
+sub by_frequency {
+  $iptable{$b} <=> $iptable{$a}
+}
